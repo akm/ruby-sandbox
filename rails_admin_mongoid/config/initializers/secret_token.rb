3cb1d024a69e3ddedfd00f867f180369e9c31c32
akm <akima@groovenauts.jp>
2012-08-21 17:33:26 +0900

    $ rails new rails_admin_mongoid -O

---
+++ rails_admin_mongoid/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAdminMongoid::Application.config.secret_token = 'cf666e0475fe316da46c3020ddc02a2c3b7e954339da53bbddfabe723686b1247cd44bca597e16839ea48a7af01732532a645b76f16e03ce0032af347e142fd3'
