89febc0b9371a246710a83e11a5fddb427e951fa
akm <akima@groovenauts.jp>
2012-08-21 16:38:38 +0900

    $ rails new rails_admin_example1

---
+++ rails_admin_example1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsAdminExample1::Application.config.secret_token = '3e3812b7c7e52f0403fbc685d63e8df18710a41ecdbb9e79fee2f723db2e2a43a469b389beafa53807a98f7ac238e3aa9693489a69b73af45e95533c3363e5b4'
---
+++ rails_admin_example1/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsAdminExample1::Application.config.session_store :cookie_store, key: '_rails_admin_example1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsAdminExample1::Application.config.session_store :active_record_store
